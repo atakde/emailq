2b53a15bf957e88477937928a5018d2338818746
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2024-01-14 15:46:47 +0300

    Change the approach for basic usage

--- src/EmailQ.php
+++ src/EmailQ.php
@@ -2,15 +2,46 @@
 
 namespace EmailQ;
 
+use Illuminate\Database\Capsule\Manager as Capsule;
 use EmailQ\Services\EmailQueue;
 
 class EmailQ
 {
+    private array $config;
     private EmailQueue $emailQueue;
 
-    public function __construct()
+    public function __construct(array $config = [])
     {
+        $this->setConfig($config);
+        $this->initDBConnection();
         $this->emailQueue = new EmailQueue();
+    }
+
+    public function setConfig(array $config)
+    {
+        $this->config = $config;
+    }
+
+    public function initDBConnection()
+    {
+        if (empty($this->config)) {
+            throw new \Exception('Configurations are not set');
+        }
+
+        $capsule = new Capsule();
+        $capsule->addConnection([
+            'driver' => $this->config['DB_DRIVER'],
+            'host' => $this->config['DB_HOST'],
+            'database' => $this->config['DB_NAME'],
+            'username' => $this->config['DB_USER'],
+            'password' => $this->config['DB_PASSWORD'],
+            'charset' => $this->config['DB_CHARSET'],
+            'collation' => $this->config['DB_COLLATION'],
+            'prefix' => '',
+        ]);
+
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
     }
 
     public function queue($params)
